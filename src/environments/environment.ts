b4cdc0319d3455800c428c9532f0429ee645e8ae
thulanigamtee <76236208+thulanigamtee@users.noreply.github.com>
2023-10-21 19:55:07 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   firebase: {
-    projectId: 'expense-tracker-d7230',
-    appId: '1:619777498432:web:d8940b257ab452e2e52ea5',
-    storageBucket: 'expense-tracker-d7230.appspot.com',
-    apiKey: 'AIzaSyCH8eLNN73q5Eyd0XKf7ZnclX8MAmxNiX0',
-    authDomain: 'expense-tracker-d7230.firebaseapp.com',
-    messagingSenderId: '619777498432',
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
   },
 };
